163ab8fd2421412594f27b28fa0bea31807f9fd3
candrabiyu4 <candrabiyu4@gmail.com>
2021-06-24 21:45:20 +0700

    gantu tutor

--- resources/views/tentor/listTentor.blade.php
+++ resources/views/tentor/listTentor.blade.php
@@ -32,7 +32,7 @@
                     <div class="col-md-12">
                         <div class="card">
                             <div class="card-header">
-                                <h4 class="card-title">Data Tentor</h4>
+                                <h4 class="card-title">Data Tutor</h4>
                             </div>
                             <div class="card-body">
                                 <div class="table-responsive">
